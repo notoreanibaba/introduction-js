dd1d485e1a1ea37fa3800d95ef4aba7764aeda0c
Oreoluwa Anibaba <105011991+notoreanibaba@users.noreply.github.com>
2022-06-21 13:48:20 +0100

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,9 @@
+
+let myName = "Oreoluwa";
+let height = "5'6";
+let country = "Nigeria";
+let bio = `Name: ${myName}, Height: ${height}, Country: ${country}.`;
+
+console.log(bio);
+document.write(`<h1>${bio}</h1>`)
+alert(bio)
